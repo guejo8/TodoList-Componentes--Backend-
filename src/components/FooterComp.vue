015488352001c8e90fb1db13ac9127bffd4f9f76
YorlinPacha <yorlin1517@gmail.com>
2023-04-17 18:47:21 +0200

    Primera carga de la aplicacion

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="text-center text-lg-start">
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2023 Copyright / vue-3 / powered by:
+    <a  href="https://www.linkedin.com/in/yorlinpachajoa-dev/"> Yorlin Pachajoa</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+footer, a{
+    color: #fff;
+}
+</style>
